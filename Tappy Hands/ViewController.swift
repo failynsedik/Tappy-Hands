7fa46e129a04590ea2f564e8ca41fa1fb8dc4187
Failyn Kaye Sedik <failynsedik@gmail.com>
2020-10-30 17:49:14 +0800

    Initial Commit

---
+++ Tappy Hands/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tappy Hands
+//
+//  Created by Failyn Kaye M. Sedik on 10/30/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
