20792d63d4918862935fccd2395bdd95bd985466
Failyn Kaye Sedik <failynsedik@gmail.com>
2020-10-31 13:03:13 +0800

    Add HomeViewController as the app's root view controller

---
+++ Tappy Hands/View/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  Tappy Hands
+//
+//  Created by Failyn Kaye M. Sedik on 10/31/20.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+    
+}
