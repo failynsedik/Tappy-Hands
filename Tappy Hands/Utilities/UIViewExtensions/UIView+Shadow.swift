88ed761342f527b7a81cb99ca0eea51b6c661110
Failyn Kaye Sedik <failynsedik@gmail.com>
2020-11-03 21:55:01 +0800

    Update implementation of adding shadow

--- Tappy Hands/Utilities/UIViewExtensions/UIView+Shadow.swift
+++ Tappy Hands/Utilities/UIViewExtensions/UIView+Shadow.swift
@@ -23,10 +23,15 @@ extension UIView {
         color: UIColor,
         shadowOpacity: Float
     ) {
-        self.layer.shadowOffset = CGSize(width: x, height: y)
-        self.layer.shadowColor = color.cgColor
         self.layer.shadowOpacity = shadowOpacity
+        self.layer.shadowColor = color.cgColor
+        self.layer.shadowOffset = CGSize(width: x, height: y)
         self.layer.shadowRadius = blur
+        
+        // Render and cache the layer
+        self.layer.shouldRasterize = true
+        // Make sure the cache is retina (the default is 1.0)
+        self.layer.rasterizationScale = UIScreen.main.scale
     }
     
 }
